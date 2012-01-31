7a26194ff456b185fac6b3f5ad79216628a84b0d
John <jonathan.detchart@gmail.Com>
2012-01-31 18:01:17 +0100

    Forget to init the SSLContext with the custom "allowAll" TrustManager

--- src/de/timroes/axmlrpc/utils/CustomSSLSocketFactory.java
+++ src/de/timroes/axmlrpc/utils/CustomSSLSocketFactory.java
@@ -4,20 +4,52 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 
 
 public class CustomSSLSocketFactory extends javax.net.ssl.SSLSocketFactory{
 
-	SSLContext sslContext;
-	
+	private SSLContext sslContext;
+	private static TrustManager trustManager = new X509TrustManager()
+	{
+
+		@Override
+		public void checkClientTrusted(X509Certificate[] chain, String authType)
+				throws CertificateException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void checkServerTrusted(X509Certificate[] chain, String authType)
+				throws CertificateException {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public X509Certificate[] getAcceptedIssuers() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+		
+	};
 	public CustomSSLSocketFactory()
 	{
-		try {
+		try {			
 			sslContext = SSLContext.getInstance("TLS");
+			sslContext.init(null, new TrustManager[] { trustManager }, null);
 		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (KeyManagementException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
